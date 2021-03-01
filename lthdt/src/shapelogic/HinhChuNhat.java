d58cd295af1c1a3566afe4a40e7501f9d6137679
USER <USER@DESKTOP-N4I6O0M>
2021-03-01 23:28:35 +0700

    Bài thiết kế 01 02

---
+++ lthdt/src/shapelogic/HinhChuNhat.java
@@ -0,0 +1,27 @@
+package shapelogic;
+
+public class HinhChuNhat extends HinhHoc {
+
+    private int lower_x;
+
+    private int lower_y;
+
+    private int upper_x;
+
+    private int upper_y;
+
+    public HinhChuNhat() {
+    }
+
+    @Override
+    public double tinhDienTic() {
+        return super.tinhDienTic(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double tinhChuVi() {
+        return super.tinhChuVi(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
---
+++ lthdt/src/shapelogic/HinhTron.java
@@ -0,0 +1,25 @@
+package shapelogic;
+
+public class HinhTron extends HinhHoc {
+
+    private int x;
+
+    private int y;
+
+    private double r;
+
+    public HinhTron() {
+    }
+
+    @Override
+    public double tinhDienTic() {
+        return super.tinhDienTic(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double tinhChuVi() {
+        return super.tinhChuVi(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}

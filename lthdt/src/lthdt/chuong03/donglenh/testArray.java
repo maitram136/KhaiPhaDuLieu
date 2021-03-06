3921fdbe70f864037e41c1a3de4b348cca22d06e
USER <USER@DESKTOP-N4I6O0M>
2021-03-06 17:06:57 +0700

    Bài 13: Mảng 1 chiều (kiểu dữ liệu cơ bản)

---
+++ lthdt/src/lthdt/chuong03/donglenh/testArray.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lthdt.chuong03.donglenh;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class testArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        int[] a;
+        //khoi tao
+//        a = new int[4];
+//        a[0] = 12;
+//        a[1] = 9;
+//        a[2] = 1;
+//        a[1] = 15;
+        //xuat du lieu
+//        System.out.println(a.toString());
+//        System.out.println(Arrays.toString(a));
+          
+//          int[] b = new int[]{1, 3, -7, 8, 9, 20};
+//          System.out.println(Arrays.toString(b));
+//          System.out.println("Số lượng phần tử của mảng là " + b.length);
+          
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Số lượng phần tử của mảng là n = ");
+          String input = sc.nextLine();
+          int n = Integer.parseInt(input);
+          int[] c = new int[n];
+          Random rnd = new Random();
+          for(int i = 0; i < n; i ++){
+              c[i] = rnd.nextInt(1000);
+          }
+          System.out.println(Arrays.toString(c));
+//          for(int i = 0; i < n; i++){
+//          System.out.print("c[" + i + "] = ");
+//          input = sc.nextLine();
+//          c[i] = Integer.parseInt(input);
+//          }
+//          System.out.println("Giá trị của mảng vừa nhập là ");
+//          System.out.println(Arrays.toString(c));
+//          for(int i = 0; i < c.length; i ++){
+//              System.out.print(c[i] + " ");
+//          }
+    }
+    
+}
